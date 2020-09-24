e3eb58c20ea27a57ab6ba6b2c586d85f522dc76d
yusa_kanata <876203144@qq.com>
2020-09-24 20:41:11 +0800

    完善样式

--- src/pages/Homepage/components/Nav/index.jsx
+++ src/pages/Homepage/components/Nav/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Button } from 'antd';
+import { Switch, Button, Menu, Dropdown } from 'antd';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
@@ -11,8 +11,23 @@ function Nav(props) {
     props.limitHandle(checked);
   };
 
+  const orderMenu = (
+    <Menu>
+      <Menu.Item>默认</Menu.Item>
+      <Menu.Item>收藏</Menu.Item>
+      <Menu.Item>评分</Menu.Item>
+      <Menu.Item>时间</Menu.Item>
+    </Menu>
+  );
+
   return (
     <nav>
+      <span style={{ float: 'right' }}>
+        {/* TODO: 排序功能 */}
+        <Dropdown overlay={orderMenu}>
+          <Button type="primary">排序方式</Button>
+        </Dropdown>
+      </span>
       <Switch
         onChange={(checked) => {
           handleSwitch(checked);
@@ -23,14 +38,13 @@ function Nav(props) {
       >
         Limit 模式
       </Switch>
+      {/* TODO: 搜索功能 */}
       <Button>搜索</Button>
       {!login && (
         <Link to="/upload">
           <Button type="primary">上传</Button>
         </Link>
       )}
-      {/* TODO: 新建排序方式 */}
-      <div>排序方式</div>
     </nav>
   );
 }
